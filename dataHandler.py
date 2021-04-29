63adc0299a4923fd1a210380753d3c51ea76889b
artik19129 <48104129+artik19129@users.noreply.github.com>
2021-04-30 01:44:56 +0300

    add DataBase

---
+++ dataHandler.py
@@ -0,0 +1,8 @@
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
